ce1148495fd564afa22ef14cc30a755cdad8c2b6
Devesh <deveshchhabra333@gmail.com>
2025-04-09 16:09:44 +0530

    ShoppingSite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './component/header'
+import Main from './component/main'
+const App = () => {
+  return (
+    <div>
+
+      <Header/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+const header = () => {
+    return (
+        <header>
+            <div className='header-container  '>
+                <div className='header-title '>Grey Scientific Labs</div>
+                <div className='header-links'>
+                <div className='header-link'>Cart</div>
+                <div className='header-link'>Login</div>
+               </div>
+            </div>
+
+        </header>
+    )
+}
+
+export default header
